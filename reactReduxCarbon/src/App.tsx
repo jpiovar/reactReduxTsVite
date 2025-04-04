b1ec2736f612f7bc286dd459c02ad4145dc43274
Juraj Piovar <jpiovar@sk.ibm.com>
2025-04-04 11:56:04 +0200

    bar and linear echart

--- reactReduxCarbon/src/App.tsx
+++ reactReduxCarbon/src/App.tsx
@@ -7,7 +7,7 @@ import * as echarts from "echarts";
 import ChartComponent from "./components/ChartComponent/chartComponent";
 
 function App() {
-  const chartOptions: echarts.EChartsOption = {
+  const barChartOptions: echarts.EChartsOption = {
     xAxis: {
       type: "category",
       data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
@@ -21,14 +21,50 @@ function App() {
         type: "bar",
       },
     ],
+    tooltip: {
+      trigger: 'axis', // Show tooltip on axis hover
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}, // Add save as image button
+      },
+    },
   };
+
+  const linearChartOptions: echarts.EChartsOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'line', // Use 'line' for a linear chart
+        smooth: true, // Optional: smooth the line
+        areaStyle: {}, // Optional: Add area under the line
+      },
+    ],
+    tooltip: {
+      trigger: 'axis', // Show tooltip on axis hover
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}, // Add save as image button
+      },
+    },
+  };
+
 
   return (
     <>
       <div>
-        <div style={{ width: "800px", height: "400px", margin: "auto" }}>
+        <div style={{ width: "800px", height: "1000px", margin: "auto" }}>
           <h1>ECharts in React TypeScript</h1>
-          <ChartComponent options={chartOptions} />
+          <ChartComponent options={barChartOptions} />
+          <ChartComponent options={linearChartOptions} />
         </div>
         <Stack gap={7}>
           <Button>Click Me1</Button>
